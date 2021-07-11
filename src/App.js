30666f2cb53fb7f2b87ae7796d9782441dd32163
Tíäg Silvä <70734516+tiag188@users.noreply.github.com>
2021-07-11 16:18:11 -0300

    fix: corrigir nomemclatura do Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro";
 import ListDeNotas from "./components/ListaDeNotas";
 
-class App extends Components {
+class App extends Component {
   render() {
     return (
       <section>
