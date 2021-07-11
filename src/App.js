2defe0c4a36fd0981cdf310ed7405fd365a1e1a1
Tíäg Silvä <70734516+tiag188@users.noreply.github.com>
2021-07-11 18:07:28 -0300

    feat: incluir estilo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import FormularioCadastro from "./components/FormularioCadastro";
-import ListDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import ListDeNotas from "./components/ListaDeNotas/ListaDeNotas";
+import "./assets/App.css"
 
 class App extends Component {
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListDeNotas />
       </section>
