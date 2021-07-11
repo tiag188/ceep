56cac3854c6489029a7d356fed0e5d56669c7c79
Tíäg Silvä <70734516+tiag188@users.noreply.github.com>
2021-07-11 16:14:51 -0300

    feat: criar componente cardnota

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
+import CardNota from "../components/CardNota";
 
 class ListDeNotas extends Component {
   render() {
     return (
       <ul>
         <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-              <p>Escreva sua nota</p>
-            </header>
-          </section>
+          <CardNota />
         </li>
         <li>
-          <section>
-            <header>
-              <h3>Título</h3>
-              <p>Escreva sua nota</p>
-            </header>
-          </section>
+          <CardNota />
         </li>
       </ul>
     );
